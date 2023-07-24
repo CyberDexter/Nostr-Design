3da9be9718ff8a2fbc405616b86219e037495234
TheGrinder <2921922+CyberDexter@users.noreply.github.com>
2023-07-24 06:56:19 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://nostrdesign.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Nostr-Design/build/index.html/',
+  baseUrl: '/Nostr-Design/build/index.html',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
